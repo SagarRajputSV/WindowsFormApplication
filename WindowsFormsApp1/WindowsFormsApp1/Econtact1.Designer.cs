db36fdb113f49bab4c463c4ea33bb6a1f8692e9b
sagar <sagartubearn@gmail.com>
2019-06-24 19:02:57 +0530

    Version 1.0 24-06-2019

---
+++ WindowsFormsApp1/WindowsFormsApp1/Econtact1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Econtact1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Econtact1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtboxName = new System.Windows.Forms.TextBox();
+            this.txtboxContact = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtboxEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(453, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(335, 204);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(70, 33);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // txtboxName
+            // 
+            this.txtboxName.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxName.Location = new System.Drawing.Point(492, 203);
+            this.txtboxName.Name = "txtboxName";
+            this.txtboxName.Size = new System.Drawing.Size(232, 34);
+            this.txtboxName.TabIndex = 2;
+            // 
+            // txtboxContact
+            // 
+            this.txtboxContact.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxContact.Location = new System.Drawing.Point(492, 272);
+            this.txtboxContact.Name = "txtboxContact";
+            this.txtboxContact.Size = new System.Drawing.Size(232, 34);
+            this.txtboxContact.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(335, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Contact No.";
+            // 
+            // txtboxEmail
+            // 
+            this.txtboxEmail.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxEmail.Location = new System.Drawing.Point(492, 347);
+            this.txtboxEmail.Name = "txtboxEmail";
+            this.txtboxEmail.Size = new System.Drawing.Size(232, 34);
+            this.txtboxEmail.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(335, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "EmailID";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(453, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(998, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 27);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Econtact1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1044, 640);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtboxEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtboxContact);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Econtact1";
+            this.Text = "Econtact";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtboxName;
+        private System.Windows.Forms.TextBox txtboxContact;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Econtact1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Econtact1 : Form
+    {
+        public Econtact1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "server=10.0.103.99\\SQL2008R2;database=SQL_Training1;UID=training;Password=training";
+                SqlConnection con = new SqlConnection(str);
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "UpdateTheVisit";
+                cmd.Connection = con;
+
+                if(txtboxEmail == null)
+                {
+                    cmd.Parameters.AddWithValue("@Name", txtboxName.Text);
+                    cmd.Parameters.AddWithValue("@ContactNo", txtboxContact.Text);
+                    SqlParameter output = new SqlParameter();
+
+                    output.ParameterName = "@ret";
+                    output.SqlDbType = SqlDbType.VarChar;
+                    output.Direction = ParameterDirection.Output;
+
+                    cmd.Parameters.Add(output);
+
+                    string result = output.Value.ToString();
+                    MessageBox.Show(result);
+                }
+
+                else
+                {
+                    cmd.Parameters.AddWithValue("@Name", txtboxName.Text);
+                    cmd.Parameters.AddWithValue("@ContactNo", txtboxContact.Text);
+                    cmd.Parameters.AddWithValue("@EmailId", txtboxEmail.Text);
+
+                    SqlParameter output = new SqlParameter();
+
+                    output.ParameterName = "@ret";
+                    output.SqlDbType = SqlDbType.VarChar;
+                    output.Direction = ParameterDirection.Output;
+
+                    cmd.Parameters.Add(output);
+
+                    string result = output.Value.ToString();
+                    MessageBox.Show(result);
+                }
+            }
+
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+    }
+}
